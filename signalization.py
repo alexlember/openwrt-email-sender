41a8440647e605bdb23528e19089a331ee7118b2
Алексей Лемберский <alexlember@gmail.com>
2016-09-29 00:19:11 +0300

    signalization final

--- signalization.py
+++ signalization.py
@@ -1,92 +1,109 @@
 import os
 
+import time
 
-# Метод, в котором происходит установка входного порта.
+DEBUG = True
+
+
+# Input port setup.
 def set_gpios():
-    os.system("echo XX > /sys/class/gpio/export")
-    os.system("echo 'echo XX > /sys/class/gpio/export' >> log")
-    os.system("echo in > /sys/class/gpio/gpioXX/direction")
-    os.system("echo 'echo in > /sys/class/gpio/gpioXX/direction' >> log")
+    cmd = "echo XX > /sys/class/gpio/export"
+    os.system(cmd)
+    logger(cmd)
+
+    cmd = "echo in > /sys/class/gpio/gpioXX/direction"
+    os.system(cmd)
+    logger(cmd)
 
 
-# Метод, который считывает текущее состояние входного порта.
+# Method reading current state of input.
 def get_gpio_state():
-    f = os.popen("cat /sys/class/gpio/gpioXX/value")
+    f = os.popen("cat /sys/class/gpio/gpioXXS/value")
     return str(f.read())
 
 
-# Метод, который отправляет письмо.
+# Main send mail method (on state change)
 def send_email(state_change):
     form_message_body(state_change)
-    f = os.popen("mailsend -to user@gmail.com -from user@gmail.com -starttls -port 587 -auth -smtp"
-                 " smtp.gmail.com -sub 'Signalization alert' +cc +bc -v -user user@gmail.com "
+    f = os.popen("mailsend -to alexlember@gmail.com -from novokosino.home@gmail.com -starttls -port 587 -auth -smtp"
+                 " smtp.gmail.com -sub 'Signalization alert' +cc +bc -v -user novokosino.home@gmail.com "
                  "-pass 'pass' -mime-type 'text/html' -msg-body ./message-body.html")
+
     result = str(f.read())
-    os.system("echo '" + result + "' >> log")
-    os.system("echo "" > message-body.html")
+    logger(result)
+    logger("echo "" > message-body.html")
 
 
-# Метод формирует тело письма.
+# Method for form message-body
 def form_message_body(state_change):
-
     cmd = "cat warning.html > message-body.html"
-    print "cmd: " + cmd
     os.system(cmd)
-    os.system("echo '" + cmd + "' >> log")
+    logger(cmd)
 
-    cmd = "echo '<b>' >> message-body.html"
-    print "cmd: " + cmd
-    os.system(cmd)
-    os.system("echo '" + cmd + "' >> log")
+    if state_change.strip() == str("01"):
+        cmd = "cat zero_to_one.html >> message-body.html"
+        os.system(cmd)
+        logger(cmd)
 
-    cmd = "echo '" + state_change + "' >> message-body.html"
-    print "cmd: " + cmd
-    os.system(cmd)
-    os.system("echo '" + cmd + "' >> log")
-
-    cmd = "echo '</b>' >> message-body.html"
-    print "cmd: " + cmd
-    os.system(cmd)
-    os.system("echo '" + cmd + "' >> log")
-
-    cmd = "echo '<br>' >> message-body.html"
-    print "cmd: " + cmd
-    os.system(cmd)
-    os.system("echo '" + cmd + "' >> log")
-
-    cmd = "echo '<br>' >> message-body.html"
-    print "cmd: " + cmd
-    os.system(cmd)
-    os.system("echo '" + cmd + "' >> log")
+    elif state_change.strip() == str("10"):
+        cmd = "cat one_to_zero.html >> message-body.html"
+        os.system(cmd)
+        logger(cmd)
 
     cmd = "date >> message-body.html"
-    print "cmd: " + cmd
     os.system(cmd)
-    os.system("echo '" + cmd + "' >> log")
-
-    print ""
-    print ""
+    logger(cmd)
 
 
-# Основная функция, где осуществляется настройка и дальнейший поллинг gpio.
+# Greeting method send main at startup.
+def send_start_email():
+    cmd = "cat start-message-body.html > message-body.html"
+    os.system(cmd)
+    logger(cmd)
+
+    cmd = "date >> message-body.html"
+    os.system(cmd)
+    logger(cmd)
+
+    f = os.popen("mailsend -to alexlember@gmail.com -from novokosino.home@gmail.com -starttls -port 587 -auth -smtp"
+                 " smtp.gmail.com -sub 'Signalization alert' +cc +bc -v -user novokosino.home@gmail.com "
+                 "-pass 'pass' -mime-type 'text/html' -msg-body ./message-body.html")
+
+    result = str(f.read())
+    logger(result)
+    logger("echo "" > message-body.html")
+
+
+# Event logger
+def logger(message):
+    os.system("echo '" + message + "' >> log")
+    os.system("date >> log")
+    os.system("echo "" >> log")
+
+    if DEBUG:
+        print message
+        print time.strftime("%H:%M:%S")
+        print ""
+
+
+# Main function, basic init, gpio poll.
 def main():
-	set_gpios()
-	previous_input_state = get_gpio_state().strip()
-	current_input_state = get_gpio_state().strip()
-	os.system("date >> message-body.html")
-	print "setup completed"
-	var = 1
-	while var == 1:
-		previous_input_state = current_input_state
-		current_input_state = get_gpio_state().strip()
-		if current_input_state != previous_input_state:
-			print ""
-			print ""
-			state = "%s -> %s" % (previous_input_state, current_input_state)
-			print "State changed: " + state
-			os.system("echo 'State changed: " + state + "' >> log")
-			print send_email(state)
-			
-      
-# Запуск основного цикла
+
+    set_gpios()
+    current_input_state = get_gpio_state().strip()
+    logger("current state: " + current_input_state)
+    send_start_email()
+    logger("setup completed")
+
+    var = 1
+    while var == 1:
+        previous_input_state = current_input_state
+        current_input_state = get_gpio_state().strip()
+        if current_input_state != previous_input_state:
+            state = "%s%s" % (previous_input_state, current_input_state)
+            logger("State changed: " + state)
+            send_email(state)
+
+
+# Main cycle start
 main()
